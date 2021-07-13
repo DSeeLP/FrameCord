8b81cdb9da13f0653c7d71392cbf44ea6631fa29
DSeeLP <46624152+dseelp@users.noreply.github.com>
2021-07-13 13:04:42 +0200

    Events aren't printed anymore to the console

--- core/src/main/kotlin/de/dseelp/kotlincord/core/listeners/EventBusListener.kt
+++ core/src/main/kotlin/de/dseelp/kotlincord/core/listeners/EventBusListener.kt
@@ -9,15 +9,8 @@ import de.dseelp.kotlincord.api.Bot
 import de.dseelp.kotlincord.api.InternalKotlinCordApi
 import de.dseelp.kotlincord.api.event.EventBus
 import de.dseelp.kotlincord.api.utils.koin.CordKoinComponent
-import dev.kord.core.event.Event
-import dev.kord.core.on
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.subscribe
-import kotlinx.coroutines.launch
 import org.koin.core.component.inject
 
 @OptIn(InternalKotlinCordApi::class)
@@ -27,7 +20,6 @@ object EventBusListener: CordKoinComponent {
 
     init {
         bot.kord.events.onEach {
-            println(it)
             eventBus.callAsync(it)
         }.launchIn(bot.kord)
     }
