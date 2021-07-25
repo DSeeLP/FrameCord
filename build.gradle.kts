1518ce56efc4e63d3f34b6eaea289fdc4883889b
DSeeLP <46624152+dseelp@users.noreply.github.com>
2021-07-26 00:11:48 +0200

    Update Dokka to 1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
     java
     `maven-publish`
     signing
-    id("org.jetbrains.dokka") version "1.4.32" apply false
+    id("org.jetbrains.dokka") version "1.5.0" apply false
 
     kotlin("jvm") version "1.5.21" apply false
     id("com.github.johnrengelman.shadow") version "6.1.0" apply false
